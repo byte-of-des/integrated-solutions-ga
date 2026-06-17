877366159905f46863469b852ab897c5af537068
Destiny Colley <destiny.colley@icloud.com>
2026-06-17 19:21:24 -0400

    fix: bypass smooth-scroll on route change so scroll-to-top works reliably

--- components/ScrollToTop/ScrollToTop.tsx
+++ components/ScrollToTop/ScrollToTop.tsx
@@ -4,8 +4,17 @@ import { usePathname } from 'next/navigation'
 
 export default function ScrollToTop() {
   const pathname = usePathname()
+
   useEffect(() => {
-    window.scrollTo(0, 0)
+    // Disable smooth-scroll temporarily so we jump instantly, not animate
+    document.documentElement.style.scrollBehavior = 'auto'
+    document.documentElement.scrollTop = 0
+    document.body.scrollTop = 0
+    // Re-enable on next frame
+    requestAnimationFrame(() => {
+      document.documentElement.style.scrollBehavior = ''
+    })
   }, [pathname])
+
   return null
 }
